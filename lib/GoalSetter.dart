6a9c9f7f5e0e2cc01f7fd647c9a679314b57af14
James Cowell <59481864+jamesecowell@users.noreply.github.com>
2020-04-27 16:00:48 +0100

    Create goal (#9)
    * began setting up GoalSetter page
    
    * added and styled image container and text inputs in GoalSetter
    
    * added 'next' button to GoalSetter
    
    * added form to GoalSetter
    
    * added image to GoalSetter
    
    * added image
    
    * added form validation
    
    Co-authored-by: blueca <58993642+blueca@users.noreply.github.com>

--- lib/GoalSetter.dart
+++ lib/GoalSetter.dart
@@ -1,12 +1,153 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
 
-class GoalSetter extends StatefulWidget {
+class GoalSetter extends StatelessWidget {
   @override
-  _GoalSetterState createState() => _GoalSetterState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoalForm(),
+      backgroundColor: Colors.purple,
+    );
+  }
 }
 
-class _DicePageState extends State<GoalSetter> {
+class GoalForm extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {}
+  _GoalFormState createState() {
+    return _GoalFormState();
+  }
+}
+
+class _GoalFormState extends State<GoalForm> {
+  String _petName;
+  String _goalName;
+
+  final _formKey = GlobalKey<FormState>();
+
+  // TODO: Add better layout/constraints, handle form submit
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'hi james, select the creature you will take care of',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.cyan,
+              ),
+            ),
+          ),
+          Container(
+            width: 117.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.grey,
+            ),
+            child: Image.asset('images/pixil-cat.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'name the creature',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.cyan,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a name!';
+                }
+              },
+              onSaved: (String value) {
+                _petName = value;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey,
+                hintText: 'enter a name...',
+                hintStyle: TextStyle(
+                  fontFamily: 'PressStart2P',
+                ),
+              ),
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'what is your overall goal?',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.cyan,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a goal!';
+                }
+              },
+              onSaved: (String value) {
+                _goalName = value;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey,
+                hintText: 'enter a goal...',
+                hintStyle: TextStyle(
+                  fontFamily: 'PressStart2P',
+                ),
+              ),
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+              ),
+            ),
+          ),
+          FlatButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                print('form is valid');
+                _formKey.currentState.save();
+              }
+              print(_petName);
+              print(_goalName);
+            },
+            child: Text(
+              'NEXT',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                fontSize: 24,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
