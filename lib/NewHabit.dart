cf7ccdc7bcddbc283cf35fa0239bc95e3dcb4ee4
blueca <nickoldacre@outlook.com>
2020-04-27 12:57:45 +0100

    formatting code for NewHabit

--- lib/NewHabit.dart
+++ lib/NewHabit.dart
@@ -10,7 +10,6 @@ class NewHabit extends StatelessWidget {
   }
 }
 
-// Create a Form widget.
 class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
@@ -18,19 +17,11 @@ class MyCustomForm extends StatefulWidget {
   }
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
 class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: Column(
@@ -89,10 +80,7 @@ class MyCustomFormState extends State<MyCustomForm> {
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: FlatButton(
               onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
                 if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a Snackbar.
                   Scaffold.of(context).showSnackBar(
                       SnackBar(content: Text('Submitting Habit...')));
                 }
