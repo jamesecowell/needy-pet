194af6d3b2f8ea1b7bdd8a818f3fe0c2a23a86dc
blueca <nickoldacre@outlook.com>
2020-04-27 16:06:15 +0100

    on press talking to database

--- lib/NewHabit.dart
+++ lib/NewHabit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NewHabit extends StatelessWidget {
   @override
@@ -19,6 +20,8 @@ class MyCustomForm extends StatefulWidget {
 
 class MyCustomFormState extends State<MyCustomForm> {
   final _formKey = GlobalKey<FormState>();
+  final newHabitController = TextEditingController();
+  final dbRef = Firestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +55,7 @@ class MyCustomFormState extends State<MyCustomForm> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
+              controller: newHabitController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(
@@ -81,6 +85,21 @@ class MyCustomFormState extends State<MyCustomForm> {
             child: FlatButton(
               onPressed: () {
                 if (_formKey.currentState.validate()) {
+                  addNewHabit(newHabitController.text);
+                  // dbRef.push().set({"habit": newHabitController}).then((_) {
+                  //   Scaffold.of(context).showSnackBar(
+                  //     SnackBar(
+                  //       content: Text('Habit Added!'),
+                  //     ),
+                  //   );
+                  //   newHabitController.clear();
+                  // }).catchError((onError) {
+                  //   Scaffold.of(context).showSnackBar(
+                  //     SnackBar(
+                  //       content: Text(onError),
+                  //     ),
+                  //   );
+                  // });
                   Scaffold.of(context).showSnackBar(
                       SnackBar(content: Text('Submitting Habit...')));
                 }
@@ -97,5 +116,12 @@ class MyCustomFormState extends State<MyCustomForm> {
         ],
       ),
     );
+  }
+
+  void addNewHabit(habit) async {
+    await dbRef.collection('new_habit').document().setData({'habit': habit});
+    DocumentReference ref =
+        await dbRef.collection('books').add({'habit': habit});
+    print(ref.documentID);
   }
 }
