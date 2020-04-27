0c35b77721b16dcc2bc5c5907809567640f691c1
James Cowell <james.cowell28@gmail.com>
2020-04-27 10:15:30 +0100

    began setting up GoalSetter page

---
+++ lib/GoalSetter.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
+class GoalSetter extends StatefulWidget {
+  @override
+  _GoalSetterState createState() => _GoalSetterState();
+}
+
+class _DicePageState extends State<GoalSetter> {
+  @override
+  Widget build(BuildContext context) {}
+}
