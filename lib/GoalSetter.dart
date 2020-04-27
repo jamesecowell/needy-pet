2d585290cbbc914079b29c5b7572bea59d034811
James Cowell <james.cowell28@gmail.com>
2020-04-27 12:36:02 +0100

    added and styled image container and text inputs in GoalSetter

--- lib/GoalSetter.dart
+++ lib/GoalSetter.dart
@@ -21,6 +21,77 @@ class _GoalSetterState extends State<GoalSetter> {
               ),
             ),
           ),
+          Container(
+            width: 117.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.grey,
+            ),
+            // child: Image.asset('images/pixil-cat.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'name the creature',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.cyan,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey,
+                hintText: 'enter a name...',
+                hintStyle: TextStyle(
+                  fontFamily: 'PressStart2P',
+                ),
+              ),
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'what is your overall goal?',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.cyan,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey,
+                hintText: 'enter a goal...',
+                hintStyle: TextStyle(
+                  fontFamily: 'PressStart2P',
+                ),
+              ),
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+              ),
+            ),
+          ),
         ],
       ),
     );
