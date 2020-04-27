4af052115b28df56525dd8aff16f83a935cae0e3
Harriet Melesi <harrietmelesi@harriets-air.lan>
2020-04-27 15:47:41 +0100

    fix xcode config

---
+++ lib/Welcome.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() {
+    return _WelcomeState();
+  }
+}
+
+class _WelcomeState extends State<Welcome> {
+  String _welcomemessage = 'welcome user! keep pet alive!'
+  @override
+  Widget _buildBody(BuildContext context) {
+    final itemTile = new Text("$_welcomemessage");
+    return 
+    
+    
+    
+  }
+
