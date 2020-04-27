e8fd136ae5ea34c5d2e3e51fec167c7f72ba7f82
blueca <nickoldacre@outlook.com>
2020-04-27 12:56:49 +0100

    formatting for NewHabit

--- lib/NewHabit.dart
+++ lib/NewHabit.dart
@@ -34,41 +34,57 @@ class MyCustomFormState extends State<MyCustomForm> {
     return Form(
       key: _formKey,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text(
-            'New Habit',
-            style: TextStyle(
-              fontFamily: 'PressStart2P',
-              color: Colors.black,
-              fontSize: 25.0,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'New Habit',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.black,
+                fontSize: 25.0,
+              ),
             ),
           ),
-          Text(
-            'Please enter a habit to continue. You can add more later.',
-            style: TextStyle(
-              fontFamily: 'PressStart2P',
-              color: Colors.black,
-              fontSize: 16.0,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Please enter a habit to continue. You can add more later.',
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.black,
+                fontSize: 16.0,
+              ),
             ),
           ),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'enter habit...',
-              fillColor: Colors.grey,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.grey,
+                hintText: 'enter habit...',
+              ),
+              style: TextStyle(
+                fontFamily: 'PressStart2P',
+                color: Colors.black,
+                fontSize: 16.0,
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a habit';
+                }
+                return null;
+              },
             ),
-            style: TextStyle(
-              fontFamily: 'PressStart2P',
-              color: Colors.black,
-              fontSize: 16.0,
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter a habit';
-              }
-              return null;
-            },
           ),
+          Spacer(),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: FlatButton(
