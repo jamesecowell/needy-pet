9a9cdcc3d0204ab97515cde3c4b09b06a08520b0
blueca <nickoldacre@outlook.com>
2020-04-28 11:15:06 +0100

    [4] add username/password input boxes

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -34,7 +34,75 @@ class SignInFormState extends State<SignInForm> {
                 fontFamily: 'PressStart2P',
                 color: Colors.black,
                 fontSize: 25.0),
-          )
+          ),
+          Form(
+            key: _formKey,
+            child: Column(children: <Widget>[
+              TextFormField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintText: 'username',
+                ),
+                style: TextStyle(
+                  fontFamily: 'PressStart2P',
+                  color: Colors.black,
+                  fontSize: 16.0,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter username';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintText: 'password',
+                ),
+                style: TextStyle(
+                  fontFamily: 'PressStart2P',
+                  color: Colors.black,
+                  fontSize: 16.0,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  return null;
+                },
+              ),
+              FlatButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    print('valid sign in');
+                  }
+                },
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontFamily: 'PressStart2P',
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ]),
+          ),
         ],
       ),
     );
